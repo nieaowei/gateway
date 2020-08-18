3f18dbf7931acda6f15f556e1a8cb01efc1c5c3a
nieaowei <nieaowei@gmail.com>
2020-08-19 01:31:49 +0800

    Added swagger documentation.

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags v1.0 -o bin/gateway-linux .
 #CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -tags v1.0 -o bin/gateway-windows ./
 cp -r conf ./bin
 cp run.sh ./bin
+cp -r docs ./bin
