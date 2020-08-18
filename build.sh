ce004dea81bf14e0ba470842cf931b84773ecd95
Nekilc <nieaowei@gmail.com>
2020-08-18 16:42:44 +0800

    Updated build.sh.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -tags v1.0 -o bin/gateway-drawin ./
+#CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -tags v1.0 -o bin/gateway-drawin ./
+rm -rf ./bin/*
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -tags v1.0 -o bin/gateway-linux ./
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -tags v1.0 -o bin/gateway-windows ./
+#CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -tags v1.0 -o bin/gateway-windows ./
 cp -r conf ./bin
